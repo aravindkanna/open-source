6126915e356afd215e015338736afe1dd088c6a7
Aravind Reddy <aravindreddy255@gmail.com>
2016-04-17 18:01:33 +0530

    added bodylist()

--- a.py
+++ a.py
@@ -1,17 +1,22 @@
 class EOMMethod(object):
 	def __init__(self, fl=None, bl = None):
-		self._fl = fl
-		self._bl = bl
+		self._fl = fl if fl else []
+		self._bl = bl if bl else []
 
 	@property
 	def forcelist(self):
 		return self._fl
 
+	@property
+	def bodylist(self):
+		return self._bl
+
 class KanesMethod(EOMMethod):
 	def __init__(self, fl=None, bl=None):
-		self._fl = fl
-		self._bl = bl
+		self._fl = fl if fl else []
+		self._bl = bl if bl else []
 
 class LagrangesMethod(EOMMethod):
-	def __init__(self, fl=None):
-		self._fl = fl
+	def __init__(self, fl=None, bl=None):
+		self._fl = fl if fl else []
+		self._bl = bl if bl else []
